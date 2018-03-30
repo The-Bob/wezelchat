f34b47f9787909728db4c58551ab1101cf70bcd5
Wesley Kronmiller <wesserandlu@gmail.com>
2018-03-29 18:09:42 -0700

    add chat functionality and format

--- client/js/client.js
+++ client/js/client.js
@@ -11,6 +11,21 @@ var socket = io();
 
 nameForm.onsubmit = (e) => {
 	e.preventDefault();
+
+	console.log(nameInput.value);
+	socket.emit('nameSubmit', nameInput.value);
+
 	document.getElementById('nameScreen').style.display = 'none';
-	socket.emit('nameSubmit', chatInput.value);
+	chatInput.focus();
 }
+
+chatForm.onsubmit = (e) => {
+	e.preventDefault();
+
+	socket.emit('chatSubmit', chatInput.value);
+	chatInput.value = '';
+}
+
+socket.on('updateChat', (data) =>{
+	chatList.innerHTML += `<li id="message-${data.id}" class="message">${data.text}</li>`
+});
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 var app = express();
 var serv = require('http').Server(app);
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-   res.sendFile(__dirname + '/client/index.html');
+	res.sendFile(__dirname + '/client/index.html');
 });
 
 app.use('/client', express.static(__dirname + '/client'));
@@ -17,13 +17,18 @@ var io = require('socket.io')(serv, {});
 var socketList = new Map();
 
 io.sockets.on('connection', (socket) => {
-   console.log("YOU HAVE A USER!!!!!! \n\n\nNow that I think of it, it's probably you...\n");
+	console.log("YOU HAVE A USER!!!!!! \n\n\nNow that I think of it, it's probably you...\n");
 
-   socket.id = Math.random();
-   socketList.set(socket.id, null);
+	socket.id = Math.random();
+	socketList.set(socket.id, null);
 
-   socket.on('nameSubmit', (data) =>{
-      socketList.set(socket.id, data);
-      console.log(socketList.get(socket.id));
-   });
+	socket.on('nameSubmit', (data) =>{
+		socketList.set(socket.id, data);
+		console.log(socketList.get(socket.id) + " joined!  " + socket.id);
+	});
+
+	socket.on('chatSubmit', (data) => {
+			io.sockets.emit('updateChat', {text: `${socketList.get(socket.id)} says: ${data}`,id: Math.round(Math.random()*1000)});
+			console.log('message posted');
+	});
 });
