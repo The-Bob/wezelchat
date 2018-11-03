d0438e940d78d1618b9af89397e1f356fa50d1ce
Wesley Kronmiller <31973884+The-Bob@users.noreply.github.com>
2018-11-02 23:42:08 -0700

    More tabs to spaces

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ var serv = require('http').Server(app);
 var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/client/index.html');
+  res.sendFile(__dirname + '/client/index.html');
 });
 
 app.use(express.static(__dirname + '/client'));
@@ -17,19 +17,19 @@ var io = require('socket.io')(serv, {});
 var socketList = new Map();
 
 io.sockets.on('connection', (socket) => {
-	console.log("YOU HAVE A USER!!!!!! \n\n\nNow that I think of it, it's probably you...\n");
+  console.log("YOU HAVE A USER!!!!!! \n\n\nNow that I think of it, it's probably you...\n");
 
-	socket.id = Math.random();
-	socketList.set(socket.id, null);
+  socket.id = Math.random();
+  socketList.set(socket.id, null);
 
-	socket.on('nameSubmit', (data) =>{
-		socketList.set(socket.id, data);
-		console.log(socketList.get(socket.id) + " joined!  " + socket.id);
-	});
+  socket.on('nameSubmit', (data) =>{
+    socketList.set(socket.id, data);
+    console.log(socketList.get(socket.id) + " joined!  " + socket.id);
+  });
 
-	socket.on('chatSubmit', (data) => {
+  socket.on('chatSubmit', (data) => {
 
-		io.sockets.emit('updateChat', {text: `${socketList.get(socket.id)} says: ${data}`,id: Math.round(Math.random()*1000)});
-		console.log(`${socketList.get(socket.id)} posted ${data}`);
-	});
+    io.sockets.emit('updateChat', {text: `${socketList.get(socket.id)} says: ${data}`,id: Math.round(Math.random()*1000)});
+    console.log(`${socketList.get(socket.id)} posted ${data}`);
+  });
 });
